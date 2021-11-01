ae0eb4796f434247f7bdf282abc4a78f360a3e2c
swathidevaraplli <swathi.d118@gmail.com>
2021-11-01 16:33:06 +0530

    Difference between == and ===

---
+++ Assignmentoperatordifferenciation.js
@@ -0,0 +1,8 @@
+var one = 1;
+var one_again = 1;
+var one_string = "1";  // note: this is string
+
+console.log(one ==  one_again);  // true
+console.log(one === one_again);  // true
+console.log(one ==  one_string); // true. See below for explanation.
+console.log(one === one_string); // false. See below for explanation.
